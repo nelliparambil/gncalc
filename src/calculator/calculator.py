4f8977e80f74f90b6b72bd26c20a9e34971cbdcc
Ganesh Nelliparambil <ganeshnelliparambil@gmail.com>
2020-07-12 12:05:42 +0200

    - Reducing the size of the Calculator to 350x350 - Aligning the buttons

--- src/calculator/calculator.py
+++ src/calculator/calculator.py
@@ -4,7 +4,6 @@ from tkinter import *
 
 class Calculator(Frame):
     def __init__(self, master):
-        #.geometry('500x500')
         super(Calculator, self).__init__(master)
         self.master = master
         self.init_calculator_gui()
@@ -12,7 +11,7 @@ class Calculator(Frame):
 
     def init_calculator_gui(self):    
         self.master.title("Calculator")
-        self.master.geometry('500x500')
+        self.master.geometry('350x350')
 
         # Menubar -------------------------------------------------------------
         # File Menu
@@ -47,82 +46,79 @@ class Calculator(Frame):
                 pady=5, relief=RAISED)
         self.keypad_container.pack()
         # Calculator keys
-        btn_percent = Button(self.keypad_container, text="%", height=2,
-                padx=3, pady=2, 
+        btn_percent = Button(self.keypad_container, text="%", height=2, 
                 width=5, command=lambda: self.update_display_text('%'))
-        btn_percent.grid(row=0, column=0)
+        btn_percent.grid(row=0, column=0, padx=(3,1), pady=(3,1))
         btn_divide = Button(self.keypad_container, text="/", height=2,
-                padx=1, pady=1,
                 width=5, command=lambda: self.update_display_text('/'))        
         btn_divide.grid(row=0, column=1)
         btn_multi = Button(self.keypad_container, text="*", height=2,
-                padx=1, pady=1,
                 width=5, command=lambda: self.update_display_text('*'))
-        btn_multi.grid(row=0, column=2)
+        btn_multi.grid(row=0, column=2, padx=(3,1), pady=(3,1))
         btn_subtract = Button(self.keypad_container, text="-", height=2,
-                padx=1, pady=1,
                 width=5, command=lambda: self.update_display_text('-'))        
-        btn_subtract.grid(row=0, column=3)
+        btn_subtract.grid(row=0, column=3, padx=(3,1), pady=(3,1))
         btn_clear = Button(self.keypad_container, text="C", height=2,
-                padx=1, pady=1,
-                width=5, command=lambda: self.update_display_text('C'))        
-        btn_clear.grid(row=0, column=4)
-        btn_seven = Button(self.keypad_container, text="7",
-                command=lambda: self.update_display_text('7'))
-        btn_seven.grid(row=1, column=0)
-        btn_eight = Button(self.keypad_container, text="8",
-                command=lambda: self.update_display_text('8'))        
-        btn_eight.grid(row=1, column=1)
-        btn_nine = Button(self.keypad_container, text="9",
-                command=lambda: self.update_display_text('9'))        
-        btn_nine.grid(row=1, column=2)
-        btn_add = Button(self.keypad_container, text="+",
-                command=lambda: self.update_display_text('+'))        
-        btn_add.grid(row=1, column=3)
-        btn_ac = Button(self.keypad_container, text="AC",
-                command=lambda: self.update_display_text('AC'))        
-        btn_ac.grid(row=1, column=4)
-        btn_four = Button(self.keypad_container, text="4",
-                command=lambda: self.update_display_text('4'))        
-        btn_four.grid(row=2, column=0)
-        btn_five = Button(self.keypad_container, text="5",
-                command=lambda: self.update_display_text('5'))
-        btn_five.grid(row=2, column=1)
-        btn_six = Button(self.keypad_container, text="6",
-                command=lambda: self.update_display_text('6'))        
-        btn_six.grid(row=2, column=2)
-        btn_openbrace = Button(self.keypad_container, text="(",
-                command=lambda: self.update_display_text('('))        
-        btn_openbrace.grid(row=2, column=3)
-        btn_one = Button(self.keypad_container, text="1",
-                command=lambda: self.update_display_text('1'))        
-        btn_one.grid(row=3, column=0)
-        btn_two = Button(self.keypad_container, text="2",
-                command=lambda: self.update_display_text('2'))        
-        btn_two.grid(row=3, column=1)
-        btn_three = Button(self.keypad_container, text="3",
-                command=lambda: self.update_display_text('3'))        
-        btn_three.grid(row=3, column=2)
-        btn_equals = Button(self.keypad_container, text="=",
-                command=lambda: self.update_display_text('='))        
-        btn_equals.grid(row=3, column=3)
-        btn_closebrace = Button(self.keypad_container, text=")",
-                command=lambda: self.update_display_text(')'))        
-        btn_closebrace.grid(row=3, column=4)
-        btn_zero = Button(self.keypad_container, text="0",
-                command=lambda: self.update_display_text('0'))        
-        btn_zero.grid(row=4, column=0)
-        btn_comma = Button(self.keypad_container, text=",",
-                command=lambda: self.update_display_text(','))        
-        btn_comma.grid(row=4, column=1)
-        btn_plusorminus = Button(self.keypad_container, text="+/-",
-                command=lambda: self.update_display_text('+/-'))        
-        btn_plusorminus.grid(row=4, column=2)    
+                width=5, bg="#d65a29", 
+                command=lambda: self.update_display_text('C'))        
+        btn_clear.grid(row=0, column=4, padx=(3,1), pady=(3,1))
+        btn_seven = Button(self.keypad_container, text="7", height=2,
+                width=5, command=lambda: self.update_display_text('7'))
+        btn_seven.grid(row=1, column=0, padx=(3,1), pady=(3,1))
+        btn_eight = Button(self.keypad_container, text="8", height=2,
+                width=5, command=lambda: self.update_display_text('8'))        
+        btn_eight.grid(row=1, column=1, padx=(3,1), pady=(3,1))
+        btn_nine = Button(self.keypad_container, text="9", height=2,
+                width=5, command=lambda: self.update_display_text('9'))        
+        btn_nine.grid(row=1, column=2, padx=(3,1), pady=(3,1))
+        btn_add = Button(self.keypad_container, text="+", height=5,
+                width=5, command=lambda: self.update_display_text('+'))        
+        btn_add.grid(row=1, column=3, padx=(3,1), pady=(3,1), rowspan=2)
+        btn_ac = Button(self.keypad_container, text="AC", height=2,
+                width=5, command=lambda: self.update_display_text('AC'))        
+        btn_ac.grid(row=1, column=4,padx=(3,1), pady=(3,1))
+        btn_four = Button(self.keypad_container, text="4", height=2,
+                width=5, command=lambda: self.update_display_text('4'))        
+        btn_four.grid(row=2, column=0, padx=(3,1), pady=(3,1))
+        btn_five = Button(self.keypad_container, text="5", height=2,
+                width=5, command=lambda: self.update_display_text('5'))
+        btn_five.grid(row=2, column=1, padx=(3,1), pady=(3,1))
+        btn_six = Button(self.keypad_container, text="6", height=2,
+                width=5, command=lambda: self.update_display_text('6'))        
+        btn_six.grid(row=2, column=2, padx=(3,1), pady=(3,1))
+        btn_openbrace = Button(self.keypad_container, text="(", height=2,
+                width=5, command=lambda: self.update_display_text('('))        
+        btn_openbrace.grid(row=2, column=4, padx=(3,1), pady=(3,1))
+        btn_one = Button(self.keypad_container, text="1", height=2,
+                width=5, command=lambda: self.update_display_text('1'))        
+        btn_one.grid(row=3, column=0, padx=(3,1), pady=(3,1))
+        btn_two = Button(self.keypad_container, text="2", height=2,
+                width=5, command=lambda: self.update_display_text('2'))        
+        btn_two.grid(row=3, column=1, padx=(3,1), pady=(3,1))
+        btn_three = Button(self.keypad_container, text="3", height=2,
+                width=5, command=lambda: self.update_display_text('3'))        
+        btn_three.grid(row=3, column=2, padx=(3,1), pady=(3,1))
+        btn_equals = Button(self.keypad_container, text="=", height=5,
+                width=5, command=lambda: self.update_display_text('='))        
+        btn_equals.grid(row=3, column=3, padx=(3,1), pady=(3,1), rowspan=2)
+        btn_closebrace = Button(self.keypad_container, text=")", height=2,
+                width=5, command=lambda: self.update_display_text(')'))        
+        btn_closebrace.grid(row=3, column=4, padx=(3,1), pady=(3,1))
+        btn_zero = Button(self.keypad_container, text="0", height=2,
+                width=15, command=lambda: self.update_display_text('0'))        
+        btn_zero.grid(row=4, column=0, padx=(3,1), pady=(3,1), columnspan=2)
+        btn_comma = Button(self.keypad_container, text=",", height=2,
+                width=5, command=lambda: self.update_display_text(','))        
+        btn_comma.grid(row=4, column=2, padx=(3,1), pady=(3,1))
+        btn_plusorminus = Button(self.keypad_container, text="+/-", height=2,
+                width=5, command=lambda: self.update_display_text('+/-'))        
+        btn_plusorminus.grid(row=4, column=4, padx=(3,1), pady=(3,1))    
         
         for i in range(4):
             self.keypad_container.grid_rowconfigure(i, weight=1)
         for i in range(4):    
             self.keypad_container.grid_columnconfigure(i, weight=1)                   
+        
 
     def update_display_text(self, text):
         self.disp_text.set(text)
