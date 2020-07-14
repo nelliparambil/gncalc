2f7c879b804d5e291b9cb5eb57965a27599652e8
Ganesh Nelliparambil <ganeshnelliparambil@gmail.com>
2020-07-14 22:43:40 +0200

    Adding simple tests for Calculator
    - Adding some simple tests
    - Adding test settings
    - Fixing issue with vscode unable to watch file changes

---
+++ tests/test_calculator.py
@@ -0,0 +1,37 @@
+import unittest
+import sys
+
+from os.path import dirname, abspath, join
+
+from tkinter import Tk
+
+# Add required directories to the path
+CURRENT_DIR = dirname(__file__)
+CALC_DIR = abspath(join(CURRENT_DIR, '../src/', 'calculator'))
+sys.path.append(CALC_DIR)
+
+from calculator import Calculator
+
+class TestCalculator(unittest.TestCase):
+    def setUp(self):
+        self.root = Tk()
+        self.calc = Calculator(self.root)        
+
+
+    def test_multiple_digit_clicks(self):
+        self.calc.on_click_digit('9')
+        self.calc.on_click_digit('0')
+        self.calc.on_click_digit('3')
+        self.calc.on_click_digit('1')
+        self.assertEqual(self.calc.disp_text.get(), '9031')
+
+    def test_ingores_zoro_infront(self):
+        self.calc.on_click_digit('0')
+        self.calc.on_click_digit('0')
+        self.calc.on_click_digit('4')
+        self.calc.on_click_digit('0')
+        self.assertEqual(self.calc.disp_text.get(), '40')
+
+
+if __name__ == "__main__":
+    unittest.main()        
