a9922d0d140995281f5e25750ed38b8a242fcb36
Ganesh <ganeshnelliparambil@gmail.com>
2020-07-19 18:16:47 +0200

    Setting display var to unix:1

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,6 +1,6 @@
 import unittest
 import sys
-#import os
+import os
 
 from tkinter import Tcl
 from os.path import dirname, abspath, join
@@ -12,12 +12,13 @@ sys.path.append(CALC_DIR)
 
 from calculator import Calculator
 
-'''
+
 # To fix problem with "TclError: no display name and no $DISPLAY environment variable"
 if os.environ.get('DISPLAY','') == '':
-    print('no display found. Using :0.0')
-    os.environ.__setitem__('DISPLAY', ':0.0')
-'''
+    print('no display found. Using :0')
+    os.environ.__setitem__('DISPLAY', ':0')
+
+os.environ.__setitem__('DISPLAY', 'unix:1')
 
 class TestCalculator(unittest.TestCase):
     def setUp(self):
