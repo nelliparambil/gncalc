575cd7e64fede58fe549a9b345e5bd61c13ed7ae
Ganesh <ganeshnelliparambil@gmail.com>
2020-07-19 23:52:21 +0200

    Commenting out DISPLAY

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -13,15 +13,14 @@ sys.path.append(CALC_DIR)
 from calculator import Calculator
 
 # To fix problem with "TclError: no display name and no $DISPLAY environment variable"
-if os.environ.get('DISPLAY','') == '':
-    print('no display found. Using :0.0')
-    os.environ.__setitem__('DISPLAY', ':0.0')
+#if os.environ.get('DISPLAY','') == '':
+#    print('no display found. Using :0.0')
+#    os.environ.__setitem__('DISPLAY', ':0.0')
 
 
 class TestCalculator(unittest.TestCase):
     def setUp(self):
-        self.root = tk.Tcl()
-        self.root.loadtk()
+        self.root = tk.Tk()
         self.calc = Calculator(self.root)        
 
 
