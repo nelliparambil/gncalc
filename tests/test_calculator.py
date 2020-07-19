7ef0a3c0f8208fc6eab17fcfbe0219f84486b7f8
Ganesh <ganeshnelliparambil@gmail.com>
2020-07-19 22:43:42 +0200

    Readding display var

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -13,11 +13,10 @@ sys.path.append(CALC_DIR)
 from calculator import Calculator
 
 # To fix problem with "TclError: no display name and no $DISPLAY environment variable"
-'''
 if os.environ.get('DISPLAY','') == '':
     print('no display found. Using :0.0')
     os.environ.__setitem__('DISPLAY', ':0.0')
-'''
+
 
 class TestCalculator(unittest.TestCase):
     def setUp(self):
